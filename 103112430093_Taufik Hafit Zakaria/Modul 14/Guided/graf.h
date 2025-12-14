5fcf385d1bfa8db7ba5b476d0b354d9ac2a3bb43
Taufik Hafit Zakaria <158443028+taufuyy@users.noreply.github.com>
2025-12-14 21:07:04 +0700

    Add files via upload

---
+++ 103112430093_Taufik Hafit Zakaria/Modul 14/Guided/graf.h
@@ -0,0 +1,52 @@
+//Taufik Hafit Zakaria
+//103112430093
+//graf.h
+
+#ifndef GRAPH_H_INCLUDED
+#define GRAPH_H_INCLUDED
+
+#include <iostream>
+using namespace std;
+
+typedef char infoGraph;
+
+struct ElmNode;
+struct ElmEdge;
+
+typedef ElmNode* adrNode;
+typedef ElmEdge* adrEdge;
+
+struct ElmNode {
+    infoGraph info;
+    int visited;
+    adrEdge firstEdge;
+    adrNode next;
+};
+
+struct ElmEdge {
+    adrNode node;
+    adrEdge next;
+};
+
+struct Graph {
+    adrNode first;
+};
+
+// Primitif Graph
+void CreateGraph(Graph &G);
+adrNode AllocateNode(infoGraph X);
+adrEdge AllocateEdge(adrNode N);
+
+void InsertNode(Graph &G, infoGraph X);
+adrNode FindNode(Graph G, infoGraph X);
+
+void ConnectNode(Graph &G, infoGraph A, infoGraph B);
+
+void PrintInfoGraph(Graph G);
+
+// Traversal
+void ResetVisited(Graph &G);
+void PrintDFS(Graph &G, adrNode N);
+void PrintBFS(Graph &G, adrNode N);
+
+#endif
---
+++ 103112430093_Taufik Hafit Zakaria/Modul 14/Guided/main.cpp
@@ -0,0 +1,41 @@
+//Taufik Hafit Zakaria
+//103112430093
+//main.cpp
+
+#include "graf.h"
+#include "graf.cpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+    Graph G;
+    CreateGraph(G);
+
+    // Tambah node
+    InsertNode(G, 'A');
+    InsertNode(G, 'B');
+    InsertNode(G, 'C');
+    InsertNode(G, 'D');
+    InsertNode(G, 'E');
+
+    // Hubungkan node (Graph tidak berarah)
+    ConnectNode(G, 'A', 'B');
+    ConnectNode(G, 'A', 'C');
+    ConnectNode(G, 'B', 'D');
+    ConnectNode(G, 'C', 'E');
+
+    cout << "=== Struktur Graph ===\n";
+    PrintInfoGraph(G);
+
+    cout << "\n=== DFS dari Node A ===\n";
+    ResetVisited(G);
+    PrintDFS(G, FindNode(G, 'A'));
+
+    cout << "\n\n=== BFS dari Node A ===\n";
+    ResetVisited(G);
+    PrintBFS(G, FindNode(G, 'A'));
+
+    cout << endl;
+
+    return 0;
+}
